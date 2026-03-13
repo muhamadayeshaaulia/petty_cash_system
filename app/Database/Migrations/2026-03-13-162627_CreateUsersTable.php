9fc098209cad2ac83dd179b15d084dd0e8d20526
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-13 23:34:35 +0700

    membuat field nama lengkap di migrations users

--- app/Database/Migrations/2026-03-13-162627_CreateUsersTable.php
+++ app/Database/Migrations/2026-03-13-162627_CreateUsersTable.php
@@ -9,11 +9,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id_user' => [
+            'id_user' => [ // untuk id_user
                 'type' => 'INT',
                 'constraint' => 11,
                 'unsigned' => true,
                 'auto_increment' => true,
+            ],
+            'nama_lengkap' => [ // untuk nama lengkap
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
             ],
         ]);
     }
