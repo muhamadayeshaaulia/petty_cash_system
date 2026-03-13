c464ec54abb9954bd7e8f5e1bbf87b2db2344ba0
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-14 01:29:31 +0700

    membuat from login sederhana untuk sementara

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login - Sistem Petty Cash</title>
 </head>
-<body>
-    
+<body style="font-family: Arial, sans-serif; padding: 50px; text-align: center;">
+    <h2>Login Sistem Petty Cash <br> PT 716_Production</h2>
+    <?php if (session()->getFlashdata('error')): ?>
+        <?php endif; ?>
+
+        <form action="<? base_url('auth/process')?>"method="POST" style="display: inline-block; text-align: left;border: 1px solid #ccc; padding: 20px;">
+            <? csrf_field()?>
+            <div style="margin-bottom: 10px;">
+                <label>Username</label> <br>
+                <input type="text" name="username" required>
+            </div>
+            <div style="margin-bottom: 20px;">
+                <label>Password</label> <br>
+                <input type="password" name="password" required>
+            </div>
+            <button type="submit"></button>
+        </form>
+
 </body>
 </html>
