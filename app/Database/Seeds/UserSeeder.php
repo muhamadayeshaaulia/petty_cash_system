083f84b7c60107031e0626eab9322dfb3f309403
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-14 00:23:45 +0700

    menambahkan data dummy untuk isi tabel user

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -8,6 +8,25 @@ class UserSeeder extends Seeder
 {
     public function run()
     {
-        //
+        $data = [ //isi data dummy Tabel users
+            [
+                'nama_lengkap' => 'Staf Testing',
+                'username' => 'karyawan testing',
+                'password' => password_hash('Karyawan123', PASSWORD_DEFAULT),
+                'role' => 'karyawan',
+            ],
+            [
+                'nama_lengkap' => 'Admin Testing',
+                'username' => 'admin testing',
+                'password' => password_hash('Admin123', PASSWORD_DEFAULT),
+                'role' => 'admin_keuangan',
+            ],
+            [
+                'nama_lengkap' => 'Manager Testing',
+                'username' => 'manager testing',
+                'password' => password_hash('Manager123', PASSWORD_DEFAULT),
+                'role' => 'manager_keuangan',
+            ],
+        ];
     }
 }
