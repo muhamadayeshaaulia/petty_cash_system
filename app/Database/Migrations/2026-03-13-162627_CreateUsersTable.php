f531d4ed61e0194eaae4771c94cbe34feb8f5959
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-13 23:33:03 +0700

    membuat field id_user di migrations

--- app/Database/Migrations/2026-03-13-162627_CreateUsersTable.php
+++ app/Database/Migrations/2026-03-13-162627_CreateUsersTable.php
@@ -8,7 +8,14 @@ class CreateUsersTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_user' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+        ]);
     }
 
     public function down()
