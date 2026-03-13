3783e5ae6782c0691413f8c22a89a3c896807d17
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-14 01:00:54 +0700

    membuat proses untuk memverifikasi user dan menyimpan data kedalam session

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 use CodeIgniter\HTTP\ResponseInterface;
 
 class Auth extends BaseController
@@ -14,5 +15,34 @@ class Auth extends BaseController
             return $this->redirectBerdasarkanRole(session()->get('role'));
         }
         return view('auth/login');
+    }
+    public function process()
+    {
+        $userModel = new UserModel();
+        $username = $this->request->getPost('username');
+        $password = $this->request->getPost('password');
+
+        // cari data user berdasarkan username
+        $user = $userModel->where('username', $username)->first();
+
+        if ($user){
+            // verifikasi pass (karena di database pass di ekripsi/di hash)
+            if (password_verify((string)$password, $user['password'])){
+                // menyimpan data user kedalam session
+                $ses_data =[
+                'id_user' => $user['id_user'],
+                'nama_lengkap' => $user['nama_lengkap'],
+                'username' => $user['username'],
+                'role' => $user['role'],
+                'logged_in' => TRUE
+                ];
+                session()->set($ses_data);
+                return $this->redirectBerdasarkanRole($user['role']);
+            } else {
+                session()->setFlashdata('error', 'Username tidak di temukan!');
+                return redirect()->back();
+            }
+        }
+        
     }
 }
