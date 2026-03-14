eb01fdab0343cdc8a70c8d4f6e1d32071ba2b00d
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-15 05:42:59 +0700

    melakukan perubahan data di userseeder dan model

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -6,28 +6,48 @@ use CodeIgniter\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    public function run()
+    public function run() //data dummy
     {
-        $data = [ //isi data dummy Tabel users
-            [
-                'nama_lengkap' => 'Staf Testing',
-                'username' => 'karyawan testing',
-                'password' => password_hash('Karyawan123', PASSWORD_DEFAULT),
-                'role' => 'karyawan',
-            ],
-            [
-                'nama_lengkap' => 'Admin Testing',
-                'username' => 'admin testing',
-                'password' => password_hash('Admin123', PASSWORD_DEFAULT),
-                'role' => 'admin_keuangan',
-            ],
-            [
-                'nama_lengkap' => 'Manager Testing',
-                'username' => 'manager testing',
-                'password' => password_hash('Manager123', PASSWORD_DEFAULT),
-                'role' => 'manager_keuangan',
-            ],
-        ];
-        $this->db->table('users')->insertBatch($data);
+        //Akun Karyawan
+        $this->db->table('users')->insert([
+            'username' => 'karyawan testing',
+            'password' => password_hash('Karyawan123', PASSWORD_DEFAULT)
+        ]);
+        $userId1 = $this->db->insertID(); 
+        $this->db->table('pegawai')->insert([
+            'id_user'      => $userId1,
+            'nip'          => 'EMP-1001',
+            'email'        => 'karyawan@sariling.co.id', 
+            'nama_lengkap' => 'Staff Karyawan',
+            'role'         => 'karyawan'
+        ]);
+
+        // Akun Admin Keuangan
+        $this->db->table('users')->insert([
+            'username' => 'admin testing',
+            'password' => password_hash('Admin123', PASSWORD_DEFAULT)
+        ]);
+        $userId2 = $this->db->insertID();
+        $this->db->table('pegawai')->insert([
+            'id_user'      => $userId2,
+            'nip'          => 'ADM-2001',
+            'email'        => 'admin@sariling.co.id',
+            'nama_lengkap' => 'Admin Petty Cash',
+            'role'         => 'admin_keuangan'
+        ]);
+
+        //Akun Manager Keuangan
+        $this->db->table('users')->insert([
+            'username' => 'manager testing',
+            'password' => password_hash('Manager123', PASSWORD_DEFAULT)
+        ]);
+        $userId3 = $this->db->insertID();
+        $this->db->table('pegawai')->insert([
+            'id_user'      => $userId3,
+            'nip'          => 'MGR-3001',
+            'email'        => 'manager@sariling.co.id',
+            'nama_lengkap' => 'Manager Finance',
+            'role'         => 'manager_keuangan'
+        ]);
     }
 }
