5b3ada350888bcfe207f2b08b444cc55b4c6d4be
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2026-03-15 05:26:10 +0700

    membuat field yang akan di isi nanti nya di tabel pegawai

--- app/Database/Migrations/2026-03-14-222028_CreatePegawaiTable.php
+++ app/Database/Migrations/2026-03-14-222028_CreatePegawaiTable.php
@@ -8,7 +8,18 @@ class CreatePegawaiTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id_pegawai'   => ['type' => 'INT', 'constraint' => 11, 'unsigned' => true, 'auto_increment' => true],
+            'id_user'      => ['type' => 'INT', 'constraint' => 11, 'unsigned' => true],
+            'nip'          => ['type' => 'VARCHAR', 'constraint' => '50', 'unique' => true],
+            'email'        => ['type' => 'VARCHAR', 'constraint' => '100', 'unique' => true],
+            'nama_lengkap' => ['type' => 'VARCHAR', 'constraint' => '100'], // memindahkan nama lengkap yang awalnya ada di user jadi ke pegawai
+            'role'         => ['type' => 'ENUM', 'constraint' => ['karyawan', 'admin_keuangan', 'manager_keuangan'], 'default' => 'karyawan'],// memindahkan role yg awalnya ada di user ke tabel pegawai
+        ]);
+        
+        $this->forge->addKey('id_pegawai', true);
+        $this->forge->addForeignKey('id_user', 'users', 'id_user', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('pegawai');
     }
 
     public function down()
